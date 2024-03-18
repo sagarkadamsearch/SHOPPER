49c8dbc621a6751955350b873ac3376910064367
sagarkadamsearch <sagarkadamsearch@gmail.com>
2024-03-18 23:56:32 +0530

    changes in shop category page

--- front_end/src/Pages/ShopCategory.jsx
+++ front_end/src/Pages/ShopCategory.jsx
@@ -15,9 +15,9 @@ const ShopCategory = (props) => {
                 <p>
                     <span>Showing 1 - 12</span> out of 36 products
                 </p>
-                <div className="shopcategory-sort">
+                {/* <div className="shopcategory-sort">
                     Sort by <img src={dropdown_icon} alt="" />
-                </div>
+                </div> */}
             </div>
             <div className="shopcategory-products">
                 {all_product.map((item,index)=>{
